cdf20baefb690606decdf909e24c549d5fe77ae2
dominikvicha <161117975+dominikvicha@users.noreply.github.com>
2025-08-21 11:51:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,39 +10,95 @@ You can select range 0-10 or 0-100.
             """
 print(task_assignment)
                 
-user_choice = input("Select the range 0-10 or 0-100: or select 'q' to leave. ") 
+user_choice = input("Select the range 0-10 or 0-100 or select 'q' to leave: ") 
 
+def user_choice1(): # chybí podmínka ve které bude uživatel upozorněn na to že zvolil číslo mimo range. 
+    while True: 
+
+        if user_choice == "0-10":
+            print("Youve selected range 0-10.")
+            user_choice_number = random.randrange(11)
+            attempts_number = 0  
+
+            while True:
+                user_guess = input("Try to guess the number or 'q' to close: ")
+
+                if user_guess.lower() == "q":
+                    return
+                
+                if not user_guess.isdigit():
+                    print("You need to select just independent integer number.")
+                    continue
+
+                user_guess = int(user_guess) 
+                attempts_number += 1  
+                
+                if user_guess == user_choice_number:
+                    print("You guessed the correct number. ")
+                    break
+                if user_guess <= user_choice_number:
+                    print("Try to guess higher number.")
+                    print("Your attempt number is:", attempts_number)
+                    continue
+
+                if user_guess >= user_choice_number:
+                    print("Try to guess lower number.")
+                    print("Your attempt number is:", attempts_number)
+                    continue
+
+                else: 
+                    break
+
+def main():
+
+    while True:
+
+        if user_choice == "0-10":
+            user_choice1()
+        elif user_choice == "0-100":
+            break
+        elif user_choice == "q":
+            print("Leaving the game.")
+            break
+
+if __name__ == "__main__":
+    main()
+
+                
+
+
+"""
 while True: 
 
     if user_choice == "0-10":
         print("Youve selected range 0-10.")
         user_choice_number = random.randrange(11)
+        attempts_number = 0  
 
         while True:
             user_guess = input("Try to guess the number or 'q' to close: ")
             
             if not user_guess.isdigit():
-                print("You need to select just numbers.")
+                print("You need to select just independent integer number.")
                 continue
 
             if user_guess.lower() == "q":
                 break
 
             user_guess = int(user_guess) 
-            # po každém pokusu users guuess se přičte +1 ale musí se začínat od nuly
-            attempts_number = 0  
-
+            attempts_number += 1  
+            
             if user_guess == user_choice_number:
-                print("You guessed the correct number.")
+                print("You guessed the correct number. ")
                 break
             if user_guess <= user_choice_number:
                 print("Try to guess higher number.")
-                print("Your attempt number is:", attempts_number + 5)
+                print("Your attempt number is:", attempts_number)
                 continue
 
             if user_guess >= user_choice_number:
                 print("Try to guess lower number.")
-                print("Your attempt number is: ", attempts_number + 1)
+                print("Your attempt number is: ", attempts_number)
                 continue
 
             else: 
@@ -59,4 +115,6 @@ while True:
     else:
         break
 
+
+"""
 
