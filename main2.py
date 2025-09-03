bb4e8bc354a9e5d1562a558ae1af20a654960f9a
dominikvicha <161117975+dominikvicha@users.noreply.github.com>
2025-09-03 20:39:38 +0200

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,7 @@
+"""
+WORD GUESSING GAME 
+- user has to guess the caharcters in a randomly selected word by PC 
+- programm provides geedback after each guess
+- list  of 12 words are prepared befor the start of the game 
+
+"""
